a07040fcf2cae3ac36555ffe46f72f668a182420
Up2App <up2app4u@gmail.com>
2021-12-07 17:23:29 +0200

    add json

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,20 +1,31 @@
-import { useState } from "react";
+//react
+import { useEffect, useState } from "react";
+
+//components
 import Todo from "./todo/todo";
 
 const App = () => {
+  //state
+  const [todos, SetTodos] = useState([]);
 
-  const [todos, SetTodos] = useState([
-    {id: 1, name:'Eat', desc:'you need to eat', startDate:'2021-12-07T16:30:00', DueTo:'2021-12-07T16:40:00'},
-    {id: 2, name:'Sleep', desc:'zzzzzzzzzzzz', startDate:'2021-12-10', DueTo:'2021-12-12'},
-    {id: 3, name:'Read', desc:'wow!! great book!!', startDate:'2021-12-08T20:00:00', DueTo:'2021-12-08T21:30:00'},
-    {id: 4, name:'Drink', desc:'did i do that??', startDate:'', DueTo:''},
-    {id: 5, name:'Having Fun', desc:'whazzzzzzzzzzzzzzzzzzzzzzzzup???', startDate:'', DueTo:''},
-  ]);
+  //get all the todos from the json
+  const loadTodos = async () => {
+    let result = await fetch(`./data/todos.json`);
+    let data = await result.json();
+    
+    //call the set state function
+    SetTodos(data)
+    console.log(`data`, data);
+  }
 
+  //effect that happens only once
+  useEffect(() => {
+    loadTodos();
+  }, []);
 
   return (
     <div>
-      {todos.map((item)=><Todo todo={item}/>)}
+      {todos.map((item) => <Todo key={item.id} todo={item} />)}
     </div>
   )
 }
